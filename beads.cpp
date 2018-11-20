a8888eae86f0740e4b7bd40455dbc6fa28ae3aca
jasonapcs <42473083+jasonapcs@users.noreply.github.com>
2018-11-20 11:31:22 -0500

    Update beads.cpp

--- beads.cpp
+++ beads.cpp
@@ -11,72 +11,37 @@ PROG: beads
 
 int main() {
 	int len;
-	int currBeads = 0;
-	std::vector<int> breakPoints(1);
-	char past;
 	std::string necklace;
-	
+	int amt = 0;
+	std::vector<short int> breakPoints;
+
 	std::ifstream input("beads.in");
 	input >> len >> necklace;
 	input.close();
-	
+
+
+	len *= 2;
 	necklace += necklace;
 
+	std::string first = necklace;
+	std::string second = necklace;
+	char past = necklace[0];
 
-	past = necklace[0];
-	len *= 2;
-
-	std::string firstrun = necklace;
-
-	for (int i = 1; i < len; i++) {
-		if (necklace[i] == 'w') {
-			firstrun[i] = past;
+	for (int i = 1; i < len - 1; i++) {
+		if (necklace[i] != past && necklace[i] != 'w') {
+			breakPoints.push_back(i);
 		}
-		if ((firstrun[i] != past) && (past != 'w')) {
-			breakPoints.push_back(0);
-			breakPoints[currBeads] = i;
-			currBeads++;
-		}
-
-		past = firstrun[i];
+		past = necklace[i];
 	}
 
 	past = necklace[len - 1];
-
-	std::string secondrun = necklace;
-
-	for (int i = len - 1; i >= 0; i--) {
-		if (necklace[i] == 'w') {
-			secondrun[i] = past;
+	for (int i = len - 2; i >= 0; i--) {
+		if (necklace[i] != past && necklace[i] != 'w') {
+			breakPoints.push_back(i);
 		}
-		if ((secondrun[i] != past) && (past != 'w')) {
-			breakPoints.push_back(0);
-			breakPoints[currBeads] = i;
-			currBeads++;
-		}
-
-		past = secondrun[i];
+		past = necklace[i];
 	}
-
-	//breakPoints.erase(breakPoints.begin() + currBeads);
-
-	int amt = 0;
-	int temp = breakPoints[0] - breakPoints[breakPoints.size()] - 1;
-	amt = temp;
-
-	if (breakPoints.size() > 1)
-		if (breakPoints.size() > 2) {
-			temp = breakPoints[1] - breakPoints[0] - 1 + necklace.size() - breakPoints[breakPoints.size() / 2];
-			amt = temp;
-			for (int i = 1; i < breakPoints.size(); i++) {
-				temp = breakPoints[i + 1] - breakPoints[i - 1] - 1;
-				if (temp > amt) amt = temp;
-			}
-		}
-		else amt = temp;
-	else amt = necklace.size() / 2;
-
-
+	
 	std::ofstream output("beads.out");
 
 	output << amt << "\n";
