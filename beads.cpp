65882d0526fb4ddf959b42972bd41aee620dedcc
jasonapcs <42473083+jasonapcs@users.noreply.github.com>
2018-11-29 11:29:44 -0500

    Update beads.cpp

--- beads.cpp
+++ beads.cpp
@@ -10,46 +10,36 @@ PROG: beads
 #include <vector>
 
 int main() {
-	int len;
-	std::string necklace;
-	int amt = 0;
-	std::vector<short int> breakPoints;
-
 	std::ifstream input("beads.in");
+	int len;
+	std::vector<int> locs;
+	std::string necklace;
 	input >> len >> necklace;
 	input.close();
-
-
-	len *= 2;
-	necklace += necklace;
-
-	std::string first = necklace;
-	std::string second = necklace;
-	char past = necklace[0];
-
-	for (int i = 1; i < len - 1; i++) {
-		if (necklace[i] != past && necklace[i] != 'w') {
-			breakPoints.push_back(i);
+	necklace += necklace; len *= 2;
+	char init = '\n';
+	char currcolor = necklace[0];
+	int max = 0;
+	for (int i = 0; i < len; i++) {
+		int pos;
+		int num = 0;
+		if (necklace[i] == currcolor) {
+			num++;
 		}
-		past = necklace[i];
+		else if (necklace[i] == 'w') {
+			num++;
+		}
+		else {
+			if (num > max) max = num;
+			pos = i;
+			num = 1;
+			currcolor = necklace[i];
+		}
+
 	}
 
-	past = necklace[len - 1];
-	for (int i = len - 2; i >= 0; i--) {
-		if (necklace[i] != past && necklace[i] != 'w') {
-			breakPoints.push_back(i);
-		}
-		past = necklace[i];
-	}
-	
-	for (int i = 0; i < breakPoints.size(); i++) {
-		
-	}
-	
 	std::ofstream output("beads.out");
-
-	output << amt << "\n";
+	output << max << "\n";
 	output.close();
-
 	return 0;
 }
