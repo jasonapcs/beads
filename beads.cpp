44387a9e34d689ae60d380bbfee8ea0915da80fc
jasonapcs <42473083+jasonapcs@users.noreply.github.com>
2018-12-02 20:24:36 -0500

    MADE IT WORK (hopefully)

--- beads.cpp
+++ beads.cpp
@@ -18,88 +18,44 @@ int main() {
 	input.close();
 	necklace += necklace; len *= 2;
 	char init = '\n';
-	char currcolor = necklace[0];
-	int max = 0;
-	int pos;
 	int num = 0;
-	int bestpos;
+	int finalnum = 0;
+	
 	for (int i = 0; i < len; i++) {
-		if (necklace[i] == currcolor) {
-			num++;
-		}
-		else if (necklace[i] == 'w') {
-			num++;
-		}
-		else {
-			if (num > max) {
-				max = num;
-				bestpos = i;
+		char currcolor = necklace[i];
+		for (int j = i; j != -1; j++){
+			if (currcolor == 'w'){
+				currcolor = necklace[j];
 			}
-			pos = i;
-			num = 1;
-			currcolor = necklace[i];
-		}
-	}
-
-	currcolor = necklace[len - 1];
-	int max2 = 0;
-	int pos2;
-	int num2 = 0;
-	int bestpos2;
-	for (int i = len - 1; i >= 0; i--) {
-		if (necklace[i] == currcolor) {
-			num2++;
-		}
-		else if (necklace[i] == 'w') {
-			num2++;
-		}
-		else {
-			if (num2 > max2) {
-				max2 = num2;
-				bestpos2 = i;
+			if (necklace[j] == currcolor || necklace[j] == 'w') {
+				num++;
 			}
-			pos2 = i;
-			num2 = 1;
-			currcolor = necklace[i];
+			else {
+				j = -1;
+			}
 		}
+		for (int j = i - 1; j != -1; j--){
+			if (currcolor == 'w'){
+				currcolor = necklace[j];
+			}
+			if (necklace[j] == currcolor || necklace[j] == 'w') {
+				num++;
+			}
+			else {
+				j = -1;
+			}
+		}
+		
+		if (finalnum < num)
+			num = finalnum;
 	}
-
-	currcolor = necklace[bestpos];
-	int num3 = 0;
-	for (int i = bestpos; i != -1; i++) {
-		if (necklace[i] == currcolor) {
-			num3++;
-		}
-		else if (necklace[i] == 'w') {
-			num3++;
-		}
-		else {
-			i = -1;
-		}
-	}
-
-	int num4 = 0;
-	for (int i = bestpos; i != -1; i--) {
-		if (necklace[i] == currcolor) {
-			num4++;
-		}
-		else if (necklace[i] == 'w') {
-			num4++;
-		}
-		else {
-			i = -1;
-		}
-	}
-
-	int finalnum = num3 + num4 - 1;
-
-
-
+	
+	if (finalnum > len / 2)
+		finalnum = len / 2;
+	
 	std::ofstream output("beads.out");
 	output << finalnum << "\n";
 	output.close();
 
-	std::cout << finalnum;
-	std::cin.get();
 	return 0;
 }
